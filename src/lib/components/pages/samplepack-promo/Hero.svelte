61e0986670e9572a0b4372fb40a7d4b7499bfd3a
djkato <djkatovfx@gmail.com>
2024-11-25 21:38:45 +0100

    Revert "Merge pull request #34 from nuphory/v3-dev-november-update-2024"
    This reverts commit e36210dbb0fc4c8cc2e06ca0f09d276625450ceb, reversing
    changes made to 374923c2db0bfe7ef428b5d6613606049ba4ab67.

--- src/lib/components/pages/samplepack-promo/Hero.svelte
+++ src/lib/components/pages/samplepack-promo/Hero.svelte
@@ -17,78 +17,67 @@
 	class="z-10 absolute w-full md:w-auto right-0 bottom-0 md:right-[10vw] 3xl:right-[20vw] md:bottom-[10vh] md:skew-x-[25deg] bg-primary-side md:pl-4"
 >
 	<div
-		class="flex flex-col bg-text-primary text-black md:[&>*]:-skew-x-[25deg] px-20 py-4 pb-8 gap-4"
+		class="flex flex-col bg-text-primary text-black md:[&>*]:-skew-x-[25deg] px-20 py-4 pb-8 gap-8"
 	>
 		<h1 class="before:content-[''] mx-auto md:-mb-3 mt-3">
 			<span class="block text-4xl w-full -mb-2">HyPERTRANCE</span>
 			<span class="pl-24 block text-4xl">SAMPLEPACK</span>
 		</h1>
 
-		<div>
-			<div
-				class="w-full uppercase [&>*]:skew-x-[25deg] mt-4 mb-1 text-lg font-bold font-michroma whitespace-nowrap"
-			>
+		{#if selectedVariant.compareAtPrice !== null && selectedVariant.compareAtPrice?.amount !== selectedVariant.price?.amount}
+			<div class="flex justify-center mt-4 text-sm font-michroma -ml-3">
 				<div
-					class="w-fit bg-red-700 m-0 -translate-x-1/2 absolute left-[calc(50%-1.8rem)] -translate-y-8 md:left-0 md:translate-x-0 md:translate-y-0 md:relative md:m-0 md:ml-1 px-[1.1rem]"
+					class="flex items-center px-12 py-4 skew-x-[25deg] [&>*]:-skew-x-[25deg] bg-primary-side ring-1 ring-primary"
 				>
-					<span class="block -skew-x-[25deg] text-white"> updated november 2024 </span>
+					<div class="absolute inset-0 -top-6 -left-3">discounted price</div>
+					<Money price={selectedVariant.price} showCurrency={true} class="text-primary" />
 				</div>
+
+				<Money
+					price={selectedVariant.compareAtPrice}
+					compareAtPrice={true}
+					showCurrency={true}
+					class="flex items-center px-12 py-4 skew-x-[25deg] [&>*]:-skew-x-[25deg] ring-1 ring-primary"
+				/>
 			</div>
-			{#if selectedVariant.compareAtPrice !== null && selectedVariant.compareAtPrice?.amount !== selectedVariant.price?.amount}
-				<div class="flex justify-center text-sm font-michroma -ml-3">
-					<div
-						class="flex items-center px-12 py-4 skew-x-[25deg] [&>*]:-skew-x-[25deg] bg-primary-side ring-1 ring-primary"
-					>
-						<div class="absolute inset-0 -top-6 -left-3">discounted price</div>
-						<Money price={selectedVariant.price} showCurrency={true} class="text-primary" />
-					</div>
 
-					<Money
-						price={selectedVariant.compareAtPrice}
-						compareAtPrice={true}
-						showCurrency={true}
-						class="flex items-center px-12 py-4 skew-x-[25deg] [&>*]:-skew-x-[25deg] ring-1 ring-primary"
-					/>
-				</div>
+			<div class="flex justify-center text-xl w-full">
+				<button
+					class="hyper-button flex-1 button-primary-inverse skew-x-[25deg] [&>*]:-skew-x-[25deg]"
+					on:click={buyNow}
+				>
+					<span class="inline-block">buy now</span>
+				</button>
+				<a
+					href="/store/products/hypertrance-samplepack"
+					data-sveltekit-reload
+					class="inline-block text-bg-primary hover:text-black active:text-black py-1 px-8 m-2"
+					>learn more</a
+				>
+			</div>
+		{:else}
+			<div class="flex justify-center mt-4 text-2xl font-michroma -ml-3">
+				<button
+					class="hyper-button px-8 button-primary-inverse skew-x-[25deg] [&>*]:-skew-x-[25deg]"
+					on:click={buyNow}
+				>
+					<span class="inline-block">buy now</span>
+				</button>
 
-				<div class="flex justify-center text-xl w-full">
-					<button
-						class="hyper-button flex-1 button-primary-inverse skew-x-[25deg] [&>*]:-skew-x-[25deg]"
-						on:click={buyNow}
-					>
-						<span class="inline-block">buy now</span>
-					</button>
-					<a
-						href="/store/products/hypertrance-samplepack"
-						data-sveltekit-reload
-						class="inline-block text-bg-primary hover:text-black active:text-black py-1 px-8 m-2"
-						>learn more</a
-					>
-				</div>
-			{:else}
-				<div class="flex justify-center text-2xl font-michroma -ml-3">
-					<button
-						class="hyper-button px-8 button-primary-inverse skew-x-[25deg] [&>*]:-skew-x-[25deg]"
-						on:click={buyNow}
-					>
-						<span class="inline-block">buy now</span>
-					</button>
-
-					<Money
-						price={selectedVariant.price}
-						showCurrency={true}
-						class="flex items-center h-full px-12 py-4 skew-x-[25deg] [&>*]:-skew-x-[25deg] text-base  bg-primary-side text-primary ring-1 ring-primary"
-					/>
-				</div>
-				<div class="-mb-4 text-center md:text-end text-lg">
-					<a
-						href="/store/products/hypertrance-samplepack"
-						data-sveltekit-reload
-						class="inline-block text-bg-primary hover:text-black active:text-black py-1 px-8 m-2"
-						>learn more</a
-					>
-				</div>
-			{/if}
-		</div>
+				<Money
+					price={selectedVariant.price}
+					showCurrency={true}
+					class="flex items-center h-full px-12 py-4 skew-x-[25deg] [&>*]:-skew-x-[25deg] text-base  bg-primary-side text-primary ring-1 ring-primary"
+				/>
+			</div>
+			<div class="-mb-4 -mt-10 text-end text-lg">
+				<a
+					href="/store/products/hypertrance-samplepack"
+					data-sveltekit-reload
+					class="inline-block text-bg-primary hover:text-black active:text-black py-1 px-8 m-2"
+					>learn more</a
+				>
+			</div>
+		{/if}
 	</div>
 </div>
